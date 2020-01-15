ec98b20ecb5ced317f4e52298081538f2299e9d0
Mitchell Caisse <mitchell@mwcaisse.com>
2020-01-14 21:29:28 -0500

    Added rotating cat. Updated linting rules

--- src/index.js
+++ src/index.js
@@ -1,9 +1,32 @@
 import * as PIXI from "pixi.js"
 
 let app = new PIXI.Application({
-    width: 1024,
-    height: 1024,
+    resizeTo: window,
     antialias: true
 });
 
+//Hide the scroll bars and remove the white outline
+document.body.style.overflow = "hidden";
+document.body.style.padding = "0";
+document.body.style.margin = "0";
+
 document.body.appendChild(app.view);
+
+
+//Load up the cat
+app.loader
+    .add("cat", "assets/cat.png")
+    .load((loader, resources) => {
+        let catSprite = new PIXI.Sprite(resources.cat.texture);
+
+        catSprite.anchor.set(0.5, 0.5);
+
+        catSprite.x = app.view.width / 2;
+        catSprite.y = app.view.height / 2;
+
+        app.stage.addChild(catSprite);
+
+        app.ticker.add(() => {
+            catSprite.rotation += 0.02;
+        });
+    });
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,40 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin")
 
 module.exports = {
     entry: "./src/index.js",
     output: {
         path: path.resolve(__dirname, "build")
     },
+    module: {
+        rules: [
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader',
+                ],
+            },
+        ]
+    },
     plugins: [
         new HtmlWebpackPlugin({
             title: "SodokuJS",
             filename: "index.html"
-        })
+        }),
+        new CopyPlugin([{
+            from: "src/assets",
+            to: "assets"
+        }])
     ],
+    resolve: {
+        alias: {
+            "@app": path.resolve(__dirname, "src")
+        }
+    },
     devServer: {
         port: 3000,
         compress: true
     },
     mode: "development"
-}
+};
