ebea891a691add98f781ebecae2f048bd36f0a72
Mitchell Caisse <mitchell@mwcaisse.com>
2020-01-19 01:33:32 -0500

    Appeased the linter

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import * as PIXI from "pixi.js"
 
-import Key from "@app/Key"
-
 let app = new PIXI.Application({
     resizeTo: window,
     antialias: true
@@ -14,70 +12,70 @@ document.body.style.margin = "0";
 
 document.body.appendChild(app.view);
 
-function drawVerticalLines(app, width, height, count, x_offset, y_offset, draw_sub_lines=false) {
+function drawVerticalLines(app, width, height, count, xOffset, yOffset, drawSubLines=false) {
 
-    let spacing =  width / (count + 1);
+    let spacing = width / (count + 1);
     let color = 0x666666;
 
-    if (draw_sub_lines) {
+    if (drawSubLines) {
         color = 0xE5E5E5;
     }
 
     let i = 1;
-    for (i = 1; i <= count; i ++) {
+    for (i = 1; i <= count; i++) {
         let line = new PIXI.Graphics();
         let x = spacing * i
         line.lineStyle(4, color, 1);
-        line.moveTo(x + x_offset, 0 + y_offset);
-        line.lineTo(x + x_offset, height + y_offset);
+        line.moveTo(x + xOffset, 0 + yOffset);
+        line.lineTo(x + xOffset, height + yOffset);
         app.stage.addChild(line);
 
-        if (draw_sub_lines) {
-            drawVerticalLines(app, spacing, height, 2, x_offset + spacing * (i-1), y_offset,false);
+        if (drawSubLines) {
+            drawVerticalLines(app, spacing, height, 2, xOffset + (spacing * (i-1)), yOffset, false);
         }
     }
 
-    if (draw_sub_lines) {
-        drawVerticalLines(app, spacing, height, 2, x_offset + spacing * (i-1), y_offset,false);
+    if (drawSubLines) {
+        drawVerticalLines(app, spacing, height, 2, xOffset + (spacing * (i-1)), yOffset, false);
     }
 
 }
 
-function drawHorizontalLines(app, width, height, count, x_offset, y_offset, draw_sub_lines=false) {
+function drawHorizontalLines(app, width, height, count, xOffset, yOffset, drawSubLines=false) {
 
-    let spacing =  height / (count + 1);
+    let spacing = height / (count + 1);
     let color = 0x666666;
 
-    if (draw_sub_lines) {
+    if (drawSubLines) {
         color = 0xE5E5E5;
     }
 
     let i = 1;
-    for (i = 1; i <= count; i ++) {
+    for (i = 1; i <= count; i++) {
         let line = new PIXI.Graphics();
         line.lineStyle(4, color, 1);
-        line.moveTo(0 + x_offset, i * spacing + y_offset);
-        line.lineTo(width + x_offset, i * spacing + y_offset);
+        line.moveTo(0 + xOffset, (i * spacing) + yOffset);
+        line.lineTo(width + xOffset, (i * spacing) + yOffset);
         app.stage.addChild(line);
 
-        if (draw_sub_lines) {
-            drawHorizontalLines(app, width, spacing, 2, x_offset,y_offset + spacing * (i - 1), false);
+        if (drawSubLines) {
+            drawHorizontalLines(app, width, spacing, 2, xOffset, yOffset + (spacing * (i - 1)), false);
         }
     }
 
-    if (draw_sub_lines) {
-        drawHorizontalLines(app, width, spacing, 2, x_offset,y_offset + spacing * (i - 1), false);
+    if (drawSubLines) {
+        drawHorizontalLines(app, width, spacing, 2, xOffset, yOffset + (spacing * (i - 1)), false);
     }
 
 }
 
 function drawBoard(app, x, y, width, height) {
-    drawVerticalLines(app, width, height, 2, x,y, true);
-    drawHorizontalLines(app, width, height, 2,x,y, true );
+    drawVerticalLines(app, width, height, 2, x, y, true);
+    drawHorizontalLines(app, width, height, 2, x, y, true);
 
-    //draw the board outline
+    //Draw the board outline
 
-    //horizontal outlines
+    //Horizontal outlines
     let line = new PIXI.Graphics();
     //0xE5E5E5
     line.lineStyle(4, 0xE5E5E5, 1);
@@ -86,13 +84,12 @@ function drawBoard(app, x, y, width, height) {
     app.stage.addChild(line);
 
     line = new PIXI.Graphics();
-    //0xE5E5E5
     line.lineStyle(4, 0xE5E5E5, 1);
     line.moveTo(x + width, 0 + y);
     line.lineTo(x + width, height + y);
     app.stage.addChild(line);
 
-    //vertical outlines
+    //Vertical outlines
     line = new PIXI.Graphics();
     line.lineStyle(4, 0xE5E5E5, 1);
     line.moveTo(x, y);
@@ -104,18 +101,12 @@ function drawBoard(app, x, y, width, height) {
     line.moveTo(x, y + height);
     line.lineTo(x + width, y + height);
     app.stage.addChild(line);
-
-
 }
 
 /*eslint-disable max-lines-per-function */
 /*eslint-disable max-statements */
-function setup(loader, resources) {
-
-    drawBoard(app, 200,200, 800,800);
-
-
-
+function setup() {
+    drawBoard(app, 200, 200, 800, 800);
 }
 
 //Load up the cat
