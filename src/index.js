923eaa635eaf7fbd5cbbb2ce6e7733b6f3846bc5
Mitchell Caisse <mitchell@mwcaisse.com>
2020-01-19 01:24:06 -0500

    Added a sudoku board

--- src/index.js
+++ src/index.js
@@ -14,74 +14,111 @@ document.body.style.margin = "0";
 
 document.body.appendChild(app.view);
 
+function drawVerticalLines(app, width, height, count, x_offset, y_offset, draw_sub_lines=false) {
+
+    let spacing =  width / (count + 1);
+    let color = 0x666666;
+
+    if (draw_sub_lines) {
+        color = 0xE5E5E5;
+    }
+
+    let i = 1;
+    for (i = 1; i <= count; i ++) {
+        let line = new PIXI.Graphics();
+        let x = spacing * i
+        line.lineStyle(4, color, 1);
+        line.moveTo(x + x_offset, 0 + y_offset);
+        line.lineTo(x + x_offset, height + y_offset);
+        app.stage.addChild(line);
+
+        if (draw_sub_lines) {
+            drawVerticalLines(app, spacing, height, 2, x_offset + spacing * (i-1), y_offset,false);
+        }
+    }
+
+    if (draw_sub_lines) {
+        drawVerticalLines(app, spacing, height, 2, x_offset + spacing * (i-1), y_offset,false);
+    }
+
+}
+
+function drawHorizontalLines(app, width, height, count, x_offset, y_offset, draw_sub_lines=false) {
+
+    let spacing =  height / (count + 1);
+    let color = 0x666666;
+
+    if (draw_sub_lines) {
+        color = 0xE5E5E5;
+    }
+
+    let i = 1;
+    for (i = 1; i <= count; i ++) {
+        let line = new PIXI.Graphics();
+        line.lineStyle(4, color, 1);
+        line.moveTo(0 + x_offset, i * spacing + y_offset);
+        line.lineTo(width + x_offset, i * spacing + y_offset);
+        app.stage.addChild(line);
+
+        if (draw_sub_lines) {
+            drawHorizontalLines(app, width, spacing, 2, x_offset,y_offset + spacing * (i - 1), false);
+        }
+    }
+
+    if (draw_sub_lines) {
+        drawHorizontalLines(app, width, spacing, 2, x_offset,y_offset + spacing * (i - 1), false);
+    }
+
+}
+
+function drawBoard(app, x, y, width, height) {
+    drawVerticalLines(app, width, height, 2, x,y, true);
+    drawHorizontalLines(app, width, height, 2,x,y, true );
+
+    //draw the board outline
+
+    //horizontal outlines
+    let line = new PIXI.Graphics();
+    //0xE5E5E5
+    line.lineStyle(4, 0xE5E5E5, 1);
+    line.moveTo(x, y);
+    line.lineTo(x, height + y);
+    app.stage.addChild(line);
+
+    line = new PIXI.Graphics();
+    //0xE5E5E5
+    line.lineStyle(4, 0xE5E5E5, 1);
+    line.moveTo(x + width, 0 + y);
+    line.lineTo(x + width, height + y);
+    app.stage.addChild(line);
+
+    //vertical outlines
+    line = new PIXI.Graphics();
+    line.lineStyle(4, 0xE5E5E5, 1);
+    line.moveTo(x, y);
+    line.lineTo(x + width, y);
+    app.stage.addChild(line);
+
+    line = new PIXI.Graphics();
+    line.lineStyle(4, 0xE5E5E5, 1);
+    line.moveTo(x, y + height);
+    line.lineTo(x + width, y + height);
+    app.stage.addChild(line);
+
+
+}
+
 /*eslint-disable max-lines-per-function */
 /*eslint-disable max-statements */
 function setup(loader, resources) {
-    let catSprite = new PIXI.Sprite(resources.cat.texture);
 
-    catSprite.anchor.set(0.5, 0.5);
+    drawBoard(app, 200,200, 800,800);
 
-    catSprite.x = app.view.width / 2;
-    catSprite.y = app.view.height / 2;
 
-    app.stage.addChild(catSprite);
 
-    let down = new Key("ArrowDown"),
-        left = new Key("ArrowLeft"),
-        right = new Key("ArrowRight"),
-        up = new Key("ArrowUp");
-
-    catSprite.speed = 5;
-    catSprite.vx = 0;
-    catSprite.vy = 0;
-
-    left.addListener((pressed) => {
-        if (pressed) {
-            catSprite.vx = -1 * catSprite.speed;
-            console.log("LEFT PRESSED");
-        }
-        else {
-            catSprite.vx = 0;
-        }
-    });
-
-    right.addListener((pressed) => {
-        if (pressed) {
-            catSprite.vx = catSprite.speed;
-        }
-        else {
-            catSprite.vx = 0;
-        }
-    });
-
-    up.addListener((pressed) => {
-        if (pressed) {
-            catSprite.vy = -1 * catSprite.speed;
-        }
-        else {
-            catSprite.vy = 0;
-        }
-    });
-
-    down.addListener((pressed) => {
-        if (pressed) {
-            catSprite.vy = catSprite.speed;
-        }
-        else {
-            catSprite.vy = 0;
-        }
-    });
-
-    catSprite.tick = (delta) => {
-        catSprite.x += catSprite.vx * delta;
-        catSprite.y += catSprite.vy * delta;
-    };
-
-    app.ticker.add(catSprite.tick);
 }
 
 //Load up the cat
 app.loader
-    .add("cat", "assets/cat.png")
     .load(setup);
 
